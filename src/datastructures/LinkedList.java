f851b1e4d2ba57d76f745893eaceed3b3b3630e0
Mohammad Reza <M.Jafaryan2006@gmail.com>
2025-06-02 11:49:39 +0330

    Deploying linked list

---
+++ src/datastructures/LinkedList.java
@@ -0,0 +1,131 @@
+package datastructures;
+
+public class LinkedList<T> {
+    private class Node {
+        public T data;
+        public Node link;
+
+        public Node() {
+            this.data = null;
+            this.link = null;
+        }
+
+        public Node(T data) {
+            this.data = data;
+            this.link = null;
+        }
+    }
+
+    private Node headNode;
+    private int length;
+
+    public LinkedList() {
+        this.headNode = new Node();
+        this.length = 0;
+    }
+
+    public void addNode(T data) {
+        addNode(data, length - 1);
+    }
+
+    public void addNode(T data, int place) throws IndexOutOfBoundsException {
+        if (place >= this.length)
+            throw new IndexOutOfBoundsException();
+
+        Node perviousNode = this.headNode;
+        Node currentNode = this.headNode.link;
+
+        for (int i = 0; i < place; i++) {
+            perviousNode = currentNode;
+            currentNode = currentNode.link;
+        }
+
+        perviousNode.link = new Node(data);
+        perviousNode.link.link = currentNode;
+        this.length++;
+    }
+
+    public T getNode() {
+        return this.headNode.link.data;
+    }
+
+    public T getNode(int place) throws IndexOutOfBoundsException {
+        if (place > this.length)
+            throw new IndexOutOfBoundsException();
+
+        Node currentNode = this.headNode.link;
+        for (int i = 0; i < place; i++)
+            currentNode = currentNode.link;
+
+        return currentNode.data;
+    }
+
+    public void deleteNode() {
+        Node firstDataNode = this.headNode.link;
+        this.headNode.link = firstDataNode.link;
+        firstDataNode.link = null;
+        this.length--;
+    }
+
+    public void deleteNode(int place) throws IndexOutOfBoundsException {
+        if (place >= this.length)
+            throw new IndexOutOfBoundsException();
+
+        Node perviousNode = this.headNode;
+        Node currentNode = this.headNode.link;
+
+        for (int i = 0; i < place; i++) {
+            perviousNode = currentNode;
+            currentNode = currentNode.link;
+        }
+
+        perviousNode.link = currentNode.link;
+        currentNode.link = null;
+        this.length--;
+    }
+
+    public void deleteNode(T data) {
+        Node perviousNode = this.headNode;
+        Node currentNode = this.headNode.link;
+
+        while (currentNode != null) {
+            if (currentNode.data == data) {
+                perviousNode.link = currentNode.link;
+                currentNode.link = null;
+                this.length--;
+            }
+
+            perviousNode = currentNode;
+            currentNode = currentNode.link;
+        }
+    }
+
+    public int isInList(T data) {
+        Node currentNode = this.headNode.link;
+        int count = 0;
+
+        while (currentNode != null) {
+            if (currentNode.data.equals(data))
+                return count;
+
+            currentNode = currentNode.link;
+            count++;
+        }
+        return -1;
+    }
+
+    public T getData(T data) { // Maybe you think it's useless but no, in this time you know this using
+        Node currentNode = this.headNode.link;
+        while (currentNode != null) {
+            if (currentNode.data.equals(data))
+                return currentNode.data;
+
+            currentNode = currentNode.link;
+        }
+        return null;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+}
