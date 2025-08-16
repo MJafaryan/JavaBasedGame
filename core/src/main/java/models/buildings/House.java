172698f1fad01ece065b95db1887dd8db8c79613
M.Jafaryan <m.jafaryan2006@yahoo.com>
2025-08-16 11:16:49 +0330

    deploy the house

--- core/src/main/java/models/buildings/House.java
+++ core/src/main/java/models/buildings/House.java
@@ -3,9 +3,13 @@ package models.buildings;
 import org.json.simple.JSONObject;
 import datastructures.SimplerJson;
 import models.user.Colony;
+import models.Basics;
+import models.persons.*;
 
-public class House extends Building {
+public class House extends Building implements Runnable {
+    private int maxPopulation;
     private int population;
+    private Person[] personsArray;
 
     public House(Colony colony) throws Exception {
         super(colony);
@@ -14,11 +18,38 @@ public class House extends Building {
         payCost((JSONObject) SimplerJson.getDataFromJson(config, "cost"));
 
         this.health = (int) (long) SimplerJson.getDataFromJson(config, "health");
-        this.colony.setMaximumPossiblePopulation((int) (long) SimplerJson.getDataFromJson(config, "capacity") + this.colony.getMaximumPossiblePopulation());
+        this.maxPopulation = (int) (long) SimplerJson.getDataFromJson(config, "capacity");
+        this.colony.setMaximumPossiblePopulation(this.maxPopulation + this.colony.getMaximumPossiblePopulation());
+        this.personsArray = new Person[this.maxPopulation];
         this.population = 0;
     }
 
     public int getPopulation() {
         return population;
+    }
+
+    public Person[] getPersons() {
+        return this.personsArray;
+    }
+
+    public void setPersons(Person[] personsArray) {
+        this.personsArray = personsArray;
+    }
+
+    public void addPerson(Person person) {
+        this.personsArray[this.population] = person;
+    }
+
+    @Override
+    public void run() {
+        while (this.population < this.maxPopulation) {
+            this.population++;
+            this.colony.setPopulation(this.colony.getPopulation() + 1);
+            try {
+                Thread.sleep(Basics.BASE_TIME_PERIOD / this.colony.getTimeCoefficient());
+            } catch (Exception e) {
+                // TODO: handle the exception
+            }
+        }
     }
 }
