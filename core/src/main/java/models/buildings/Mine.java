b46fa962db301584195fb963f3b5626875c46ccf
eisa <eisa.rasti1383@gmail.com>
2025-08-20 16:31:18 +0330

    edit Building classes

--- core/src/main/java/models/buildings/Mine.java
+++ core/src/main/java/models/buildings/Mine.java
@@ -1,13 +1,15 @@
 package models.buildings;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import models.persons.Worker;
 import models.user.Colony;
 
 public abstract class Mine extends Building {
     private Worker worker;
 
-    public Mine(Colony colony) {
-        super(colony);
+    public Mine(Texture textrue , int x , int y , int height , int weigth , String type , Colony colony) {
+        super(textrue , x, y, height, weigth, type, colony);
     }
 
     public Worker getWorker() {
